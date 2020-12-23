e73cf1d4344226c4e010d2d4beb3893884544320
nick800103 <keepprogress@gmail.com>
2020-12-23 10:02:30 +0800

    add logout support to spring security config

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -37,7 +37,9 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll();
+				.permitAll()
+			.and()
+			.logout().permitAll();
 	}
 
 	
